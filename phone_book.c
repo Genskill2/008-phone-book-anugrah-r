3ac72731e1d347416e43861ac0a502c9e90b09f4
anugrah-r <84712467+anugrah-r@users.noreply.github.com>
2021-07-07 12:23:10 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -92,9 +92,14 @@ FILE *open_db_file() {
   return fp;
 }
   
-void free_entries(entry *p) {
-  /* TBD */
-  printf("Memory is not being freed. This needs to be fixed!\n");  
+void free_entries(entry *p) 
+{
+  while(p!=NULL)
+  {
+    free(p);
+    p =p->next;
+  }
+  //printf("Memory is not being freed. This needs to be fixed!\n");  
 }
 
 void print_usage(char *message, char *progname) {
@@ -175,14 +180,20 @@ void add(char *name, char *phone) {
   fclose(fp);
 }
 
-void list(FILE *db_file) {
+void list(FILE *db_file) 
+{
   entry *p = load_entries(db_file);
   entry *base = p;
-  while (p!=NULL) {
+  int count = 0;
+  while (p!=NULL) 
+  {
     printf("%-20s : %10s\n", p->name, p->phone);
+    count++;
     p=p->next;
   }
   /* TBD print total count */
+  size =count;
+  printf("Total entries :  %d\n",count);
   free_entries(base);
 }
 
@@ -193,23 +204,48 @@ int delete(FILE *db_file, char *name) {
   entry *prev = NULL;
   entry *del = NULL ; /* Node to be deleted */
   int deleted = 0;
-  while (p!=NULL) {
-    if (strcmp(p->name, name) == 0) {
-      /* Matching node found. Delete it from the linked list.
-         Deletion from a linked list like this
-   
-             p0 -> p1 -> p2
-         
-         means we have to make p0->next point directly to p2. The p1
-         "node" is removed and free'd.
-         
-         If the node to be deleted is p0, it's a special case. 
-      */
-
-      /* TBD */
+  while (p!=NULL) 
+  {
+    if (strcmp(p->name, name) == 0) 
+    {
+     if(strcmp(base->name, name) == 0)
+       {
+         base = p->next;
+       }
+     else
+     { 
+       p = p->next;
+       prev->next = p;
+      }    
+      deleted = 1;
+      break;
+    }
+    else
+    {
+      prev = p;
+      p = p->next;
+    
     }
   }
   write_all_entries(base);
   free_entries(base);
   return deleted;
+}
+
+int search(FILE *db_file,char *name)
+{
+  entry *p = load_entries(db_file);
+  entry *base = p;
+  int found = 0;
+  while(p!=NULL)
+  {
+    if(strcmp(p->name,name) == 0)
+    {
+      printf("%s\n",p->phone);
+      found = 1;
+    }
+    p=p->next;
+  }
+  free_entries(base);
+  return found;
 }
